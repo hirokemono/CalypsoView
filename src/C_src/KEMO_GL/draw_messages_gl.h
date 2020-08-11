db038f1d1df894bbd6101a4944c16844f6fffc15
Hiroaki Matsui <h_kemono@mac.com>
2020-08-11 12:03:05 -0700

    Update message with black on white

--- src/C_src/KEMO_GL/draw_messages_gl.h
+++ src/C_src/KEMO_GL/draw_messages_gl.h
@@ -12,6 +12,7 @@
 #include "m_gl_transfer_matrix.h"
 #include "vartex_array_object_gl.h"
 #include "glsl.h"
+#include "drawGL_by_VAO.h"
 #include "coordinate_converter_c.h"
 #include "rainbow_color_code_c.h"
 #include "set_colorbar_to_buffer.h"
@@ -19,7 +20,7 @@
 
 /* prototypes */
 
-void set_message_VAO(int iflag_retina, GLint nx_win, GLint ny_win, GLfloat text_color[4],
+void set_message_VAO(int iflag_retina, GLint nx_win, GLint ny_win,
                      struct msg_work *msg_wk, struct VAO_ids *msg_VAO);
 void draw_message_VAO(struct msg_work *msg_wk,
 			struct VAO_ids *msg_VAO, struct kemoview_shaders *kemo_shaders);
